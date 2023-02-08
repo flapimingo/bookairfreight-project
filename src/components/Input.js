fdfe8439d65fdb9185c84ebae34b5c5f5dd67594
Pascal Arriagada <parriagada@jumpitt.com>
2023-02-08 03:07:19 -0300

    ♻️ Refactored input components logic

---
+++ src/components/Input.js
@@ -0,0 +1,26 @@
+import './Input.css';
+
+const Input = ({ label, type, options, value, name, onChange }) => {
+  return (
+    <div className="SelectInput">
+      {label && <label>{label}</label>}
+      {type === 'select' && options && (
+        <select value={value} onChange={onChange} name={name}>
+          {options.map((option, index) => (
+            <option value={option} key={index}>{option}</option>
+          ))}
+        </select>
+      )}
+      {['number','text'].includes(type) && (
+        <input
+          type={type}
+          name={name}
+          value={value}
+          onChange={onChange}
+        />
+      )}
+    </div>
+  );
+}
+
+export default Input;
