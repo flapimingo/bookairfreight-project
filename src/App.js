d11eb16647fe83d8bb4e350f115d6567c0ec8cce
Pascal Arriagada <parriagada@jumpitt.com>
2023-02-08 01:54:24 -0300

    🚧💄 Added SelectInput component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
+import SelectInput from './components/SelectInput';
 import TextInput from './components/TextInput';
 
 function App() {
+
+  const shippingOptions = ["Air", "Ocean"];
+
   return (
     <div className="App">
       <form>
         <TextInput text="Starting country" />
         <TextInput text="Destination country" />
         <TextInput text="Quote price" />
+        <SelectInput text="Shipping channel" options={shippingOptions}/>
       </form>
     </div>
   );
